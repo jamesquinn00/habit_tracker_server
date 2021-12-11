1f9b4d721e61e033124750121239d52278099632
Kelvin Stephano <kstephano@gmail.com>
2021-12-11 16:41:45 +0000

    edited test data

--- api/tests/integration/config.js
+++ api/tests/integration/config.js
@@ -14,8 +14,9 @@ function resetTestDB() {
             // insert test data
             db.users.insertMany([
                 { 
-                    useremail: "testUser1@email.com",
+                    userEmail: "testUser1@email.com",
                     passwordDigest: "password",
+                    userName: "test user 1",
                     habits: [
                         {
                             habitName: "Water",
@@ -28,8 +29,9 @@ function resetTestDB() {
                     ]
                 },
                 { 
-                    useremail: "testUser2@email.com",
+                    userEmail: "testUser2@email.com",
                     passwordDigest: "password",
+                    userName: "test user 2",
                     habits: [
                         {
                             habitName: "Walk the Dog",
@@ -38,19 +40,28 @@ function resetTestDB() {
                             amount: [{ expected: 2 }, { current: 1 }],
                             streak: [{ top: 10 }, { current: 10 }],
                             lastLog: "2021-12-10T11:31:21.988Z"
+                        },
+                        {
+                            habitName: "Water",
+                            frequency: 1,
+                            unit: "cups",
+                            amount: [{ expected: 6 }, { current: 4 }],
+                            streak: [{ top: 7 }, { current: 1 }],
+                            lastLog: "2021-12-11T11:31:21.988Z"
                         }
                     ]
                 },
                 { 
-                    useremail: "testUser3@email.com",
+                    userEmail: "testUser3@email.com",
                     passwordDigest: "password",
+                    userName: "test user 3",
                     habits: [
                         {
-                            habitName: "Run",
+                            habitName: "Running",
                             frequency: 7,
                             unit: "km",
                             amount: [{ expected: 5 }, { current: 2.7 }],
-                            streak: [{ top: 15 }, { current: 3 }],
+                            streak: [{ top: 15 }, { current: 15 }],
                             lastLog: "2021-12-09T11:31:21.988Z"
                         }
                     ]
