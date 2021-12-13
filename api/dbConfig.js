107cfdfed666f3ad84d593d6051b4d8f42b4b063
Kelvin Stephano <kstephano@gmail.com>
2021-12-13 13:24:51 +0000

    changed db init function to return client

--- api/dbConfig.js
+++ api/dbConfig.js
@@ -6,17 +6,12 @@ const dbName = process.env.DB_NAME; // db name
 // Create a new MongoClient
 const client = new MongoClient(dbUrl);
 
-async function initDB() {
-    try {
-        // Connect the client to the server
-        await client.connect();
-        console.log('Connected to database!', dbName);
+async function initConnection() {
+    // Connect the client to the server
+    await client.connect();
+    console.log('Connected to database:', dbName);
 
-        return client.db(dbName);
-    } finally {
-        // Ensure client will close when finished/error
-        await client.close();
-    }
+    return client;
 }
 
-module.exports = { initDB };
+module.exports = { initConnection };
