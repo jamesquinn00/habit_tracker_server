fc23939039a440819af9609ffca34cb08feeac80
Kelvin Stephano <kstephano@gmail.com>
2021-12-13 10:38:51 +0000

    added token and logout functions

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt.js');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
@@ -30,7 +30,9 @@ async function login(req, res) {
             // generate access and refresh tokens
             const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20m' });
             const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET);
-
+            // add the refresh token to user's data
+            await User.pushToken(refreshToken);
+            res.status(200).json({ accessToken: accessToken, refreshToken: refreshToken });
         } else {
             throw new Error('User could not be authenticated');
         }
@@ -38,4 +40,32 @@ async function login(req, res) {
         console.log(err);
         res.status(401).json({ err });
     }
+}
+
+async function token(req, res) {
+    const user = await User.findByEmail(req.body.email);
+    if (!user) throw new Error('No user with this email');
+    const payload = { userEmail: user.userEmail, userName: user.userName }
+    const refreshToken = req.body.token;
+
+    // check if refreshToken is null
+    if (refreshToken == null) return res.sendStatus(401)
+
+    // check is refreshToken is still valid or has been removed
+    if (!refreshToken.includes(refreshToken)) return res.sendStatus(403);
+
+    // verify the refresh token
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20m' });
+        res.status(200).json({ accessToken: accessToken });
+    });
+} 
+
+async function logout(req, res) {
+    const user = await User.findByEmail(req.body.email);
+    if (!user) throw new Error('No user with this email');
+
+    User.clearAccessTokens(user.userEmail, req.body.token);
+    res.sendStatus(204);
 }
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,5 +4,7 @@ const authController = require('../controllers/auth');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/token', authController.token);
+router.post('logout', authController.logout);
 
 module.exports = router;
