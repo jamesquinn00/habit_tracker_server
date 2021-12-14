1fa3264d90fafa4893a3fbe9df9df7ebdd4f95ac
Kelvin Stephano <kstephano@gmail.com>
2021-12-14 14:20:44 +0000

    added comment

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -6,9 +6,9 @@ function verifyToken(req, res, next) {
     // check for null token
     if (token == null) return res.sendStatus(401).json({ err: 'Null token' });
     // verify the token
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
         if (err) return res.sendStatus(403).json({ err: 'Invalid token' });
-        req.user = user;
+        req.data = data; // allow access to data in request object
         next();
     });
 }
