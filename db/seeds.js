ac85619024071f92ff0d5a67b378d9bdd76679dd
Kelvin Stephano <kstephano@gmail.com>
2021-12-14 14:21:14 +0000

    updated seeds to match db schema

--- db/seeds.js
+++ db/seeds.js
@@ -1,21 +1,27 @@
 const db = connect("mongodb://localhost:27017/habits");
 
 db.users.drop();
+db.habits.drop();
 
 db.users.insertOne(
     { 
+        id: 1,
         userEmail: "initialUser@email.com",
         passwordDigest: "password",
         refreshTokens: [],
         userName: "Initial User",
-        habits: [
-            {
-                habitName: "Water",
-                frequency: 1,
-                amount: [{ expected: 3 }, { current: 0 }],
-                streak: [{ top: 5 }, { current: 3 }],
-                lastLog: "2021-12-11T11:31:21.988Z"
-            }
-        ]
     },
-)
+);
+
+db.habits.insertOne(
+    {
+        id: 1,
+        userEmail: "initialUser@email.com",
+        userName: "Initial User",
+        habitName: "Water",
+        frequency: 1,
+        amount: [{ expected: 3 }, { current: 0 }],
+        streak: [{ top: 5 }, { current: 3 }],
+        lastLog: "2021-12-11T11:31:21.988Z"
+    }
+);
